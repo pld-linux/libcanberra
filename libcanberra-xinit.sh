de8f669c495051de40967170110067eda06498d1
Patryk Zawadzki <patrys@room-303.com>
2008-08-05 17:40:15 +0000

    - make it load
    Changed files:
        libcanberra-xinit.sh -> 1.2

--- libcanberra-xinit.sh
+++ libcanberra-xinit.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
 if [ -z "$GTK_MODULES" ] ; then
-        GTK_MODULES="libcanberra-gtk-module"
+        GTK_MODULES="libcanberra-gtk-module.so"
 else
-        GTK_MODULES="$GTK_MODULES:libcanberra-gtk-module"
+        GTK_MODULES="$GTK_MODULES:libcanberra-gtk-module.so"
 fi
 
 export GTK_MODULES
