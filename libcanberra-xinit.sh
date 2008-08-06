9ffaa3d4dbc89d7c06d3bb79c5a8154bb7abcaef
Patryk Zawadzki <patrys@room-303.com>
2008-08-06 22:20:38 +0000

    - argh, that's the correct version
    Changed files:
        libcanberra-xinit.sh -> 1.3

--- libcanberra-xinit.sh
+++ libcanberra-xinit.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
 if [ -z "$GTK_MODULES" ] ; then
-        GTK_MODULES="libcanberra-gtk-module.so"
+        GTK_MODULES="canberra-gtk-module"
 else
-        GTK_MODULES="$GTK_MODULES:libcanberra-gtk-module.so"
+        GTK_MODULES="$GTK_MODULES:canberra-gtk-module"
 fi
 
 export GTK_MODULES
